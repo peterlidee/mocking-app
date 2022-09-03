fde3351153c8385d13b23580f2633443cb673046
peterlidee <peter@lidee.be>
2022-09-03 16:22:05 +0200

    Updated the test

--- src/part2/example1/__tests__/ParentComponent.test.js
+++ src/part2/example1/__tests__/ParentComponent.test.js
@@ -1,8 +1,13 @@
-import { render } from '@testing-library/react'
+import { screen, render } from '@testing-library/react'
 import ParentComponent from '../ParentComponent'
 import ChildComponent from '../ChildComponent'
 
 jest.mock('../ChildComponent')
+
+test('ParentComponent rendered', () => {
+  render(<ParentComponent />)
+  expect(screen.getByText(/Parent Component/i)).toBeInTheDocument()
+})
 
 test('ChildComponent mock was called', () => {
   render(<ParentComponent />)
